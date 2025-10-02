601534665a34daac6fe4059d890bf9571789c9e1
choneface <gavin.garcia0001@gmail.com>
2025-10-01 22:16:47 -0400

    remove add and list functionality making edit the default

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,8 @@ mod storage;
 mod tui;
 
 use crate::storage::FileStorage;
-use clap::Parser;
-use cli::{Cli, Commands};
 
 fn main() {
-    let cli = Cli::parse();
     let storage = FileStorage::new("todo.json");
-
-    match cli.command {
-        Commands::Add {
-            description,
-            priority,
-            due,
-            tags,
-            notes,
-        } => commands::add::run(storage, description, priority, due, tags, notes),
-        Commands::List {
-            all,
-            priority,
-            tag,
-            due,
-        } => commands::list::run(storage, all, priority, tag, due),
-        Commands::Edit => commands::edit::run(storage),
-    }
+    commands::edit::run(storage);
 }
